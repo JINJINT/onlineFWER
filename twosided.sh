d5c422ccbb71711681072a4cb00bdfe8c11b306f
Jinjin Tian <jinjint@andrew.cmu.edu>
2020-09-17 19:28:44 -0400

    fix bug '

--- twosided.sh
+++ twosided.sh
@@ -2,7 +2,7 @@
 
 # specify name of experiment, which will correspond to an input file
 experiment_name="twosided"   
-for sigma in $(seq 0.1, 0.1, 1)
+for sigma in $(seq 0.1 0.1 1)
 do
 echo "Submitting job for experiment "$experiment_name" with para sigma = "$sigma
 command="./runonetwosided.sh $sigma"
