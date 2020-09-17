af0d9216067389e1f5c4a7b11829e5c75c8ea598
Jinjin Tian <jinjint@andrew.cmu.edu>
2020-09-17 19:19:26 -0400

    add sh

--- bestpara.sh
+++ bestpara.sh
@@ -8,7 +8,7 @@ do
 for lbd in $(seq 0 0.05 1)
 do 
 echo "Submitting job for experiment "$experiment_name" with para tau = "$tau" and lbd = "$lbd
-command="./runone.sh $tau $lbd"
+command="./runonebestpara.sh $tau $lbd"
 # construct final call based on machine
 sbatch --time=06:00:00 -p RM-shared -J "tau"$tau"_lbd"$lbd$experiment_name $command
 done
