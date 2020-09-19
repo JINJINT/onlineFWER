94fb415afa0b87184d191debf766bb2df0dfc828
Jinjin Tian <jinjint@andrew.cmu.edu>
2020-09-18 22:05:22 -0400

    fixbug

--- bestpara.sh
+++ bestpara.sh
@@ -8,17 +8,18 @@ do
 for lbd in $(seq 0 0.05 1)
 do 
 for FWER in $(seq 1 3 4)
-echo "Submitting job for experiment "$experiment_name" with para tau = "$tau" and lbd = "$lbd"and FWER ="$FWER
-if FWER==4
+if [ FWER==4 ]
 then
 muN=-2
+echo "Submitting job for experiment "$experiment_name" with para tau = "$tau" and lbd = "$lbd"and FWER ="$FWER ="and muN = "$muN
 command="./runonebestpara.sh $tau $lbd $FWER $muN"
 # construct final call based on machine
 sbatch --time=06:00:00 -p RM-shared -J "tau"$tau"_lbd"$lbd"_FWER"$FWER"_muN"$muN$experiment_name $command
 fi
-if FWER==1
+if [ FWER==1 ]
 then
 for muN in $(seq 0 -1 -2) 
+echo "Submitting job for experiment "$experiment_name" with para tau = "$tau" and lbd = "$lbd"and FWER ="$FWER ="and muN = "$muN
 command="./runonebestpara.sh $tau $lbd $FWER $muN"
 # construct final call based on machine
 sbatch --time=06:00:00 -p RM-shared -J "tau"$tau"_lbd"$lbd"_FWER"$FWER"_muN"$muN$experiment_name $command
