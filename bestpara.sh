d40561f47c0ad46fb7487a117d59a4bc008968e0
Jinjin Tian <jinjint@andrew.cmu.edu>
2020-09-18 22:10:57 -0400

    fixbug

--- bestpara.sh
+++ bestpara.sh
@@ -20,10 +20,12 @@ fi
 if [ FWER==1 ]
 then
 for muN in $(seq 0 -1 -2) 
+do
 echo "Submitting job for experiment "$experiment_name" with para tau = "$tau" and lbd = "$lbd"and FWER ="$FWER"and muN = "$muN
 command="./runonebestpara.sh $tau $lbd $FWER $muN"
 # construct final call based on machine
 sbatch --time=06:00:00 -p RM-shared -J "tau"$tau"_lbd"$lbd"_FWER"$FWER"_muN"$muN$experiment_name $command
+done
 fi	
 done
 done
